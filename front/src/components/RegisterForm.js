959112abfc3c67fede702ec011cf6861960fd670
charalampos <charalampos.mylothros@navd.ai>
2023-06-24 22:00:10 +0300

    add i register form also a get request to show all users registred

--- front/src/components/RegisterForm.js
+++ front/src/components/RegisterForm.js
@@ -3,10 +3,11 @@ import axios from 'axios';
 import './RegisterForm.scss';
 
 function RegisterForm() {
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [address, setAddress] = useState('');
+  const [response, setResponse] = useState('');
   const [username, setUsername] = useState('');
   const [surname, setSurname] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [address, setAddress] = useState('');
   const [errorMessagePhoneNumber, setErrorMessagePhoneNumber] = useState('');
   const [errorMessageUserName, setErrorMessageUserName] = useState('');
   const [errorMessageAddress, setErrorMessageAddress] = useState('');
@@ -26,7 +27,7 @@ function RegisterForm() {
       setErrorMessageAddress('');
     }
     
-  }, [phoneNumber, address, username, surname]);
+  }, [phoneNumber, address, username, surname, response]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -56,54 +57,75 @@ function RegisterForm() {
       } catch (error) {
         alert("Failed to register!!!");
       }
-      
+
+      try {
+        let responseGetRequest = await axios.get("http://localhost:8085/api/v1/user/get");
+        alert("GET request successful!");
+        setResponse(responseGetRequest.data);
+      } catch (error) {
+        alert("Failed to fetch data!!!");
+      }
     }
   };
 
   return (
-    <form className="register-form" onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="username">Username: </label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        {errorMessageUserName && <p className="error-message">{errorMessageUserName}</p>}
+    <div>
+      <form className="register-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username: </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          {errorMessageUserName && <p className="error-message">{errorMessageUserName}</p>}
+        </div>
+        <div>
+          <label htmlFor="surname">Surname: </label>
+          <input
+            type="text"
+            id="surname"
+            value={surname}
+            onChange={(e) => setSurname(e.target.value)}
+          />
+          {errorMessageSurname && <p className="error-message">{errorMessageSurname}</p>}
+        </div>
+        <div>
+          <label htmlFor="address">Address: </label>
+          <input
+            type="text"
+            id="address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          {errorMessageAddress && <p className="error-message">{errorMessageAddress}</p>}
+        </div>
+        <div>
+          <label htmlFor="phoneNumber">Phone Number: </label>
+          <input
+            type="text"
+            id="phoneNumber"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+          {errorMessagePhoneNumber && <p className="error-message">{errorMessagePhoneNumber}</p>}
+        </div>
+        <button type="submit">Register</button>
+      </form>
+      {response && <h1>The following users are now registered:</h1>}
+      <div className={response && "container"}>
+        {response && response.map((user) => (
+          <div key={user.userId}>
+            <p>User ID: {user.userId}</p>
+            <p>Username: {user.userName}</p>
+            <p>Surname: {user.surName}</p>
+            <p>Address: {user.address}</p>
+            <p>Phone Number: {user.phoneNumber}</p>
+          </div>
+        ))}
       </div>
-      <div>
-        <label htmlFor="surname">Surname: </label>
-        <input
-          type="text"
-          id="surname"
-          value={surname}
-          onChange={(e) => setSurname(e.target.value)}
-        />
-        {errorMessageSurname && <p className="error-message">{errorMessageSurname}</p>}
-      </div>
-      <div>
-        <label htmlFor="address">Address: </label>
-        <input
-          type="text"
-          id="address"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-        />
-        {errorMessageAddress && <p className="error-message">{errorMessageAddress}</p>}
-      </div>
-      <div>
-        <label htmlFor="phoneNumber">Phone Number: </label>
-        <input
-          type="text"
-          id="phoneNumber"
-          value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
-        />
-        {errorMessagePhoneNumber && <p className="error-message">{errorMessagePhoneNumber}</p>}
-      </div>
-      <button type="submit">Register</button>
-    </form>
+    </div>
   );
 }
 
