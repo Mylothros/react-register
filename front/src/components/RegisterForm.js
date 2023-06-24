f243caeab9df758192daed7467e23eb2ba5a7395
charalampos <charalampos.mylothros@navd.ai>
2023-06-24 20:01:59 +0300

    add the front-end of the register page

---
+++ front/src/components/RegisterForm.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './RegisterForm.scss';
+
+function RegisterForm() {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [address, setAddress] = useState('');
+  const [username, setUsername] = useState('');
+  const [surname, setSurname] = useState('');
+  const [errorMessagePhoneNumber, setErrorMessagePhoneNumber] = useState('');
+  const [errorMessageUserName, setErrorMessageUserName] = useState('');
+  const [errorMessageAddress, setErrorMessageAddress] = useState('');
+  const [errorMessageSurname, setErrorMessageSurname] = useState('');
+
+  useEffect(() => {
+    if (username !== '') {
+      setErrorMessageUserName('');
+    }
+    if (surname !== '') {
+      setErrorMessageSurname('');
+    }
+    if (phoneNumber !== '') {
+      setErrorMessagePhoneNumber('');
+    }
+    if (address !== '') {
+      setErrorMessageAddress('');
+    }
+    
+  }, [phoneNumber, address, username, surname]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (username === '') {
+      setErrorMessageUserName('Mandatory Field!');
+    }
+    if (surname === '') {
+      setErrorMessageSurname('Mandatory Field!');
+    }
+    if (phoneNumber === '') {
+      setErrorMessagePhoneNumber('Mandatory Field!');
+    }
+    if (address === '') {
+      setErrorMessageAddress('Mandatory Field!');
+    }
+    
+    let payload = {
+      userName: username,
+      surName: surname,
+      address: address,
+      phoneNumber: phoneNumber
+    }
+    if (username !== '' && surname !== '' && phoneNumber !== '' && address !== '') {
+      try {
+        let response = await axios.post("http://localhost:8085/api/v1/user/save", payload);
+        alert(response.data + " has been registered");
+      } catch (error) {
+        alert("Failed to register!!!");
+      }
+      
+    }
+  };
+
+  return (
+    <form className="register-form" onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username: </label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        {errorMessageUserName && <p className="error-message">{errorMessageUserName}</p>}
+      </div>
+      <div>
+        <label htmlFor="surname">Surname: </label>
+        <input
+          type="text"
+          id="surname"
+          value={surname}
+          onChange={(e) => setSurname(e.target.value)}
+        />
+        {errorMessageSurname && <p className="error-message">{errorMessageSurname}</p>}
+      </div>
+      <div>
+        <label htmlFor="address">Address: </label>
+        <input
+          type="text"
+          id="address"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+        {errorMessageAddress && <p className="error-message">{errorMessageAddress}</p>}
+      </div>
+      <div>
+        <label htmlFor="phoneNumber">Phone Number: </label>
+        <input
+          type="text"
+          id="phoneNumber"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        {errorMessagePhoneNumber && <p className="error-message">{errorMessagePhoneNumber}</p>}
+      </div>
+      <button type="submit">Register</button>
+    </form>
+  );
+}
+
+export default RegisterForm;
